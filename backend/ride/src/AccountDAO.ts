d703004b387b08ce54499dab2cf2c66203fa7b23
Leonardo Vales <leonardo.vales@getrak.com.br>
2024-01-04 13:55:18 -0300

    continua

--- backend/ride/src/AccountDAO.ts
+++ backend/ride/src/AccountDAO.ts
@@ -1,6 +1,8 @@
+import Account from "./Account"
+
 // port
 export default interface AccountDAO {
-  save(account: any): Promise<void>
-  getByEmail(email: string): Promise<any | undefined>
-  getById(accountId: string): Promise<any>
+  save(account: Account): Promise<void>
+  getByEmail(email: string): Promise<Account>
+  getById(accountId: string): Promise<Account>
 }
--- backend/ride/src/AccountService.ts
+++ backend/ride/src/AccountService.ts
@@ -1,5 +1,6 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
+import Account from "./Account";
 import AccountDAODatabase from "./AccountDAODatabase";
 import AccountDAO from "./AccountDAODatabase";
 import CpfValidator from "./CpfValidator";
@@ -17,22 +18,24 @@ export default class AccountService {
 
 	//port
 	async signup (input: any) {
+		const existingAccount = await this.accountDAO.getByEmail(input.email)
+		if (existingAccount) {
+			throw new Error("Account already exists");
+		}
 
-			input.accountId = crypto.randomUUID();
-			input.verificationCode = crypto.randomUUID();
-			input.date = new Date();
-			const existingAccount = await this.accountDAO.getByEmail(input.email)
-			if (existingAccount) throw new Error("Account already exists");
-			if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid name");
-			if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
-			if (!this.cpfValidator.validate(input.cpf)) throw new Error("Invalid cpf");
-			if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid plate");
-			await this.accountDAO.save(input)
-			await this.mailerGateway.send(input.email, "Verification", `Please verify your code at first login ${input.verificationCode}`);
-			return {
-				accountId: input.accountId
-			}
-
+		const account = Account.create(
+			input.name,
+			input.email,
+			input.cpf,
+			input.isPassenger,
+			input.isDriver,
+			input.carPlate
+		)
+		await this.accountDAO.save(account)
+		await this.mailerGateway.send(account.email, "Verification", `Please verify your code at first login ${account.verificationCode}`);
+		return {
+			accountId: account.accountId
+		}
 	}
 
 	//port
